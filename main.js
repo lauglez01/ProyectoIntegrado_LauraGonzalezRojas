dd7ef2fbc8b475cfe7e1dc4e99dbfca61119bf51
lauglez01 <lauragonzalez33312@gmail.com>
2024-11-22 12:07:23 +0100

    Básicamente he creado el proyecto y he comenzado a añadir los elementos del DOM en HTML. También he intentado animar un modelo3D con javascript, pero seguramente vaya a ser cambiado.

--- main.js
+++ main.js
@@ -1,24 +1,110 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'https://cdn.skypack.dev/gsap';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 13;
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+let book;
+let mixer;
+const loader = new GLTFLoader();
+loader.load('/old_magical_book.glb',
+    function (gltf) {
+        book = gltf.scene;
+        scene.add(book);
+
+        mixer = new THREE.AnimationMixer(book);
+        mixer.clipAction(gltf.animations[0]).play();
+        modelMove();
+    },
+    function (xhr) {},
+    function (error) {}
+);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
+
+const reRender3D = () => {
+    requestAnimationFrame(reRender3D);
+    renderer.render(scene, camera);
+    if(mixer) mixer.update(0.02);
+};
+reRender3D();
+
+let arrPositionModel = [
+    {
+        id: 'banner',
+        position: {x: 0, y: -1, z: 0},
+        rotation: {x: 0, y: 1.5, z: 0}
+    },
+    {
+        id: "intro",
+        position: { x: 1, y: -1, z: -5 },
+        rotation: { x: 0.5, y: -0.5, z: 0 },
+    },
+    {
+        id: "save",
+        position: { x: -1, y: -1, z: -5 },
+        rotation: { x: 0, y: 0.5, z: 0 },
+    },
+    {
+        id: "contact",
+        position: { x: 0.8, y: -1, z: 0 },
+        rotation: { x: 0.3, y: -0.5, z: 0 },
+    },
+];
+const modelMove = () => {
+    const sections = document.querySelectorAll('.section');
+    let currentSection;
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 3) {
+            currentSection = section.id;
+        }
+    });
+    let position_active = arrPositionModel.findIndex(
+        (val) => val.id == currentSection
+    );
+    if (position_active >= 0) {
+        let new_coordinates = arrPositionModel[position_active];
+        gsap.to(book.position, {
+            x: new_coordinates.position.x,
+            y: new_coordinates.position.y,
+            z: new_coordinates.position.z,
+            duration: 3,
+            ease: "power1.out"
+        });
+        gsap.to(book.rotation, {
+            x: new_coordinates.rotation.x,
+            y: new_coordinates.rotation.y,
+            z: new_coordinates.rotation.z,
+            duration: 3,
+            ease: "power1.out"
+        })
+    }
+}
+window.addEventListener('scroll', () => {
+    if (book) {
+        modelMove();
+    }
+})
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
